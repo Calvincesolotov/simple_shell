e6a38b516c0b259673ccb6be9a9e04a196d0dc6a
Calvincesolotov <solotovtechn@gmail.com>
2024-02-18 15:51:15 +0300

    a code that passes betty checks

---
+++ 0_bettyPass.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    printf("Hello, this code is betty compliant!\n");
+    return (0);
+}
